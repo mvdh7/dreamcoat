b60abc603ab8cac38bde23fdbbbaacf3c8cedc49
Matthew P. Humphreys <m.p.humphreys@icloud.com>
2022-12-28 14:49:15 +0000

    Tidy up

--- tests/test_cmems.py
+++ tests/test_cmems.py
@@ -1,9 +1,6 @@
 import numpy as np
 import dreamcoat as dc
 
-# from matplotlib import pyplot as plt
-
-
 # t8Ejkp-boxros-6qacgu-bUqmyr-i3jIls
 
 lims = dict(
@@ -14,11 +11,6 @@ lims = dict(
     latitude_min=-50,
     latitude_max=-25,
 )
-# dc.cmems.download_surphys(
-# filepath="tests/data/",
-# **lims,
-# )
-
 surface = dc.cmems.open_surface(filepath="tests/data/", **lims)
 
 speed_knots = 9
@@ -26,38 +18,9 @@ timestep_hours = 24
 ship_distance = (
     dc.convert.knots_to_kph(speed_knots) * timestep_hours * np.array([1, 2, 3])
 )
-
-# surphys['dissic'] = surbio.dissic.interp_like(surphys.theta, method='nearest')
-
-#%%
 mooring_lon_lat = [10.02, -38.41]
 
-# for i in range(surface.time.size):
-#     fig, ax = dc.plot.surface_map(
-#         # surface.mean('time'),
-#         surface.isel(time=i),
-#         "ssh",
-#         land_visible=True,
-#         ship_lon_lat=mooring_lon_lat,
-#         ship_distance=ship_distance[0],
-#         # quiver_coarsen=20,
-#         # quiver_alpha=0.1,
-#         map_extent=[5, 15, -40, -35],
-#         # vmin=13,
-#         # vmax=24,
-#         # vmin=0,
-#         # vmax=0.3,
-#         # vmin=0,
-#         # vmax=80,
-#         # vmin=7.98,
-#         # vmax=8.14,
-#         # save_figure=True,
-#         # save_path="tests/figures/{}_".format(i),
-#     )
-#     plt.show()
-#     plt.close()
-
-#%%
+#%% Daily surface maps
 dc.plot.surface_map_daily(
     surface,
     "ph",
@@ -68,20 +31,12 @@ dc.plot.surface_map_daily(
     ship_distance=ship_distance[0],
 )
 
-#%%
+#%% Time-series plots
 fvar = "dissic"
 f_surface = surface.sel(
     longitude=mooring_lon_lat[0], latitude=mooring_lon_lat[1], method="nearest"
 )
-
-fig, ax = dc.plot.surface_timeseries(f_surface, fvar)
-
-# f_surface.sel(
-#     longitude=mooring_lon_lat[0], latitude=mooring_lon_lat[1], method="nearest"
-# ).isel(depth=0)[fvar].plot(ax=ax)
-
-#%%
 dc.plot.surface_timeseries_grids(f_surface)
 
-#%% Polar
+#%% Currents at point as a polar plot
 dc.plot.surface_currents(f_surface)
