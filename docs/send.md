6f401eba953e465a56cf552604c15a79f1706baa
Matthew P. Humphreys <m.p.humphreys@icloud.com>
2023-01-10 09:44:21 +0100

    Add one file sending function

--- docs/send.md
+++ docs/send.md
@@ -4,11 +4,30 @@
 
 To send data by email with dreamcoat you will first need to set up a Gmail account with an app-specific password.  This account is used to send the emails and its username goes into the `gmail_username` kwarg below.
 
-To attach all files with a given path and extension to an email and send it, use
+### One specific file
 
 ```python
 import dreamcoat as dc
 
+dc.send.one_file(
+    filename,
+    filepath=".",
+    gmail_username=None,
+    separate=False,
+    contents=None,
+    subject="[dreamcoat] all_files_from_dir",
+    to=None,
+)
+```
+
+  * [Yagmail](https://github.com/kootenpv/yagmail) does the work behind the scenes, so see its documentation for more info about the other kwargs.
+  * Your `gmail_username` (and password) can optionally be put into the .dreamcoat directory, as can the `to` value.
+
+### All files with given extension
+
+To attach all files with a given path and extension to an email and send it, use
+
+```python
 dc.send.all_files_from_dir(
     filepath=".",
     extension="zip",
@@ -22,7 +41,6 @@ dc.send.all_files_from_dir(
 
   * Only files matching the specified `extension` are sent.
   * `separate` determines whether to send each file in a separate email or attach them all to the same one.
-  * [Yagmail](https://github.com/kootenpv/yagmail) does the work behind the scenes, so see its documentation for more info about the other kwargs.
 
 ## Tidy up after sending
 
@@ -32,4 +50,4 @@ After sending the files, you can delete all the files with a given extension fro
 dc.send.delete_from_dir(filepath=".", extension="zip")
 ```
 
-Note that this will indiscriminately delete all files in the specified `filepath` with the specified `extension` - not just the ones you sent!
+Note that this will indiscriminately delete all files in the specified `filepath` with the specified `extension` - not just the ones you sent!  Use `os.remove()` if you need to be more selective.
--- dreamcoat/send.py
+++ dreamcoat/send.py
@@ -3,6 +3,55 @@ import yagmail
 from . import meta
 
 
+def one_file(
+    filename,
+    filepath=".",
+    gmail_username=None,
+    gmail_password=None,
+    contents=None,
+    subject=None,
+    to=None,
+):
+    """Send all files in the filepath with the given extension to the given email.
+
+    Parameters
+    ----------
+    filename : str
+        The name of the file to send.
+    filepath : str, optional
+        File path to send files from, by default ".".
+    gmail_username : str, optional
+        Username of the sending gmail account, by default None, in which case this is
+        taken from the .dreamcoat .dat files or the user is prompted for input.
+    gmail_password : str, optional
+        Password of the sending gmail account, by default None, in which case it is
+        requested from the keyring or asked of the user.
+    contents : str, optional
+        Any text to go in the main body of the email, by default None.
+    subject : str, optional
+        The subject of the email, by default "[dreamcoat] {filepath}{filename}".
+    to : str, optional
+        The email address to send to, by default Nonein which case this is taken from
+        the .dreamcoat .dat files or the user is prompted for input.
+    """
+    if gmail_username is None:
+        gmail_username = meta.get_dat_data("gmail_username")
+    if gmail_password is None:
+        gmail_password = meta.get_dat_data("gmail_password")
+    if to is None:
+        to = meta.get_dat_data("to")
+    if gmail_password is None:
+        yag = yagmail.SMTP(gmail_username)
+    else:
+        yag = yagmail.SMTP(gmail_username, gmail_password)
+    if not filepath.endswith(os.sep):
+        filepath += os.sep
+    if subject is None:
+        subject = "[dreamcoat] {}{}".format(filepath, filename)
+    f = filepath + filename
+    yag.send(to=to, contents=contents, subject=subject, attachments=f)
+
+
 def all_files_from_dir(
     filepath=".",
     extension="zip",
