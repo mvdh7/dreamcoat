47b887e0aa061c8eb40fcd2317688041c9947e60
Matthew Humphreys <m.p.humphreys@icloud.com>
2024-11-16 17:56:01 +0100

    seaco2 => hseao3

--- dreamcoat/__init__.py
+++ dreamcoat/__init__.py
@@ -7,12 +7,12 @@ Dreamcoat
 Dreamcoat is a set of tools to help work with oceanographic data of the sort that might
 be needed or generated during a research expedition.
 
-Dreamcoat is being developed by Dr Matthew Humphreys (https://seaco2.group) at the NIOZ
-Royal Netherlands Institute for Sea Research, Texel.  You're very welcome to give it a
-go and suggestions and contributions are welcome, but please be aware that it is
+Dreamcoat is being developed by Dr Matthew P. Humphreys (https://hseao3.group) at the
+NIOZ Royal Netherlands Institute for Sea Research, Texel.  You're very welcome to give
+it a go and suggestions and contributions are welcome, but please be aware that it is
 primarily designed as a personal tool so there probably won't be much help if you get
 stuck; nothing is guaranteed not to break between versions, and the documentation
-(https://mvdh.xyz/dreamcoat) may be incomplete or out of date.
+(https://dreamcoat.hseao3.group) may be incomplete or out of date.
 
 The docstring examples assume that `dreamcoat` has been imported as ``dc``:
 
